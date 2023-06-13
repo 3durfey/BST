32a41dce740b7dbed0d0ee640328b1fdc1992ffe
pdurfey <3durfey@gmail.com>
2023-06-12 21:11:51 -0400

    created .js

---
+++ bst.js
@@ -0,0 +1,47 @@
+function nodeFactory() {
+  return {
+    parent: null,
+    leftChild: null,
+    rightChild: null,
+  };
+}
+function treeFactory() {
+  return {
+    buildTree(array) {
+      return;
+    },
+    insert(input) {
+      return;
+    },
+    delete(input) {
+      return;
+    },
+    find(input) {
+      return;
+    },
+    levelOrder(func) {
+      return;
+    },
+    inorder(func) {
+      return;
+    },
+    preorder(func) {
+      return;
+    },
+    postorder(func) {
+      return;
+    },
+    height(node) {
+      return;
+    },
+    depth(node) {
+      return;
+    },
+    isBalanced() {
+      return;
+    },
+    rebalance() {
+      return;
+    },
+  };
+}
