0646b0cb9922996ad91a2e11159d7cf3f214a496
pdurfey <3durfey@gmail.com>
2023-06-15 16:23:16 -0400

    before updating delete function

--- bst.js
+++ bst.js
@@ -9,36 +9,50 @@ function treeFactory() {
   return {
     array: null,
     rootNode: null,
+    height: null,
 
+    //builds tree
     buildTree(array) {
-      this.array = array;
-      this.rootNode = split(this.array);
+      this.array = array.sort((a, b) => a - b);
+      this.rootNode = this.split(this.array);
     },
+    //function to return middle number, then recursively return middle of each side till end
+    split(array) {
+      if (array.length === 0) return null;
+      if (array.length === 1) return nodeFactory(array[0], null, null);
+      //if (array.length === 2) return nodeFactory(array[1], array[0], null);
+      let root = Math.round(array.length / 2 - 1);
+      let left = [],
+        right = [];
+      for (let x = 0; x < root; x++) left.push(array[x]);
+      for (let y = root + 1; y < array.length; y++) right.push(array[y]);
+      return nodeFactory(array[root], this.split(left), this.split(right));
+    },
+
+    //inserts into tree
     insert(input) {
       let tempNode = this.rootNode;
-      let nullFound = false;
-      while (!nullFound) {
+      const newNode = nodeFactory(input, null, null);
+      while (true) {
         if (input > tempNode.value) {
           if (tempNode.rightChild === null) {
-            nullFound = true;
-            tempNode.rightChild = nodeFactory(input, null, null);
-          } else {
-            tempNode = tempNode.rightChild;
-          }
-        } else if (input < tempNode.value) {
+            tempNode.rightChild = newNode;
+            return;
+          } else tempNode = tempNode.rightChild;
+        } else {
           if (tempNode.leftChild === null) {
-            nullFound = true;
-            tempNode.leftChild = nodeFactory(input, null, null);
-          } else {
-            tempNode = tempNode.leftChild;
-          }
+            tempNode.leftChild = newNode;
+            return;
+          } else tempNode = tempNode.leftChild;
         }
       }
     },
+
     delete(input) {
       let tempNode = this.rootNode;
       let parent = this.rootNode;
       let side = "none";
+
       while (true) {
         if (input === tempNode.value) {
           //when both are null
@@ -107,20 +121,14 @@ function treeFactory() {
       let currentNode = this.rootNode;
       while (currentNode.value !== input) {
         if (input > currentNode.value) {
-          if (currentNode.rightChild === null) {
-            return "not found";
-          } else {
-            currentNode = currentNode.rightChild;
-          }
+          if (currentNode.rightChild === null) return "not found";
+          else currentNode = currentNode.rightChild;
         } else if (input < currentNode.value) {
-          if (currentNode.leftChild === null) {
-            return "not found";
-          } else {
-            currentNode = currentNode.leftChild;
-          }
+          if (currentNode.leftChild === null) return "not found";
+          else currentNode = currentNode.leftChild;
         }
       }
-      return currentNode.value + " found";
+      return currentNode;
     },
     levelOrder(func) {
       let queue = new Array();
@@ -134,91 +142,114 @@ function treeFactory() {
       return;
     },
     inorder(func) {
-      return;
+      let queue = new Array();
+      if (this.rootNode.leftChild !== null) queue.push(this.rootNode.leftChild);
+      this.traverse(queue);
+      func(this.rootNode);
+      if (this.rootNode.rightChild !== null)
+        queue.push(this.rootNode.rightChild);
+      this.traverse(queue);
     },
     preorder(func) {
-      return;
+      let queue = new Array();
+      func(this.rootNode);
+      if (this.rootNode.leftChild !== null) queue.push(this.rootNode.leftChild);
+      this.traverse(queue);
+      if (this.rootNode.rightChild !== null)
+        queue.push(this.rootNode.rightChild);
+      this.traverse(queue);
     },
     postorder(func) {
-      return;
+      let queue = new Array();
+      if (this.rootNode.leftChild !== null) queue.push(this.rootNode.leftChild);
+      this.traverse(queue);
+      if (this.rootNode.rightChild !== null)
+        queue.push(this.rootNode.rightChild);
+      this.traverse(queue);
+      func(this.rootNode);
     },
-    height(node) {
-      return;
+    traverse(queue) {
+      while (queue.length > 0) {
+        let node = queue.shift();
+        func(node);
+        if (node.leftChild !== null) queue.push(node.leftChild);
+        if (node.rightChild !== null) queue.push(node.rightChild);
+      }
     },
+    heightFunction(node) {
+      return this.heightRecursive(this.find(node.value));
+    },
+    heightRecursive(node) {
+      if (node === null) return 0;
+      const returnRight = this.heightRecursive(node.rightChild);
+      const returnLeft = this.heightRecursive(node.leftChild);
+      return Math.max(returnRight, returnLeft) + 1;
+    },
+
     depth(node) {
-      return;
+      return this.depthRecursive(node.value, this.rootNode);
+    },
+    depthRecursive(input, currentNode) {
+      if (currentNode === null) return;
+      if (input > currentNode.value) {
+        return this.depthRecursive(input, currentNode.rightChild) + 1;
+      } else if (input < currentNode.value) {
+        let returnValue = this.depthRecursive(input, currentNode.leftChild);
+        return this.depthRecursive(input, currentNode.leftChild) + 1;
+      } else return 1;
     },
     isBalanced() {
-      return;
+      const result = this.isBalancedRecursive(this.rootNode);
+      if (result < 0) return "unbalanced";
+      else return "balanced";
+    },
+    isBalancedRecursive(currentNode) {
+      if (currentNode === null) return 0;
+      const left = this.isBalancedRecursive(currentNode.leftChild);
+      if (left === -1) return -1;
+      const right = this.isBalancedRecursive(currentNode.rightChild);
+      if (right === -1) return -1;
+      if (Math.abs(right - left) > 1) return -1;
+      return Math.max(left, right) + 1;
     },
     rebalance() {
-      return;
+      let array = new Array();
+      let queue = new Array();
+      queue.push(this.rootNode);
+      while (queue.length > 0) {
+        let currentNode = queue.shift();
+        array.push(currentNode.value);
+        if (currentNode.leftChild !== null) queue.push(currentNode.leftChild);
+        if (currentNode.rightChild !== null) queue.push(currentNode.rightChild);
+      }
+      this.buildTree(array.sort((a, b) => a - b));
     },
   };
 }
-
+//running through binary tree functions
 let tree = treeFactory();
-tree.buildTree(mergeSort([1, 2, 3, 23, 33, 42, 55, 66, 70, 89]));
+tree.buildTree(arrayOfRandom(10));
+tree.insert(90);
+tree.insert(999);
+tree.insert(900);
+prettyPrint(tree.rootNode);
+tree.rebalance();
+prettyPrint(tree.rootNode);
 
-//prettyPrint(tree.rootNode);
-let count = 0;
-function func(input) {
-  count++;
-  console.log(count);
-}
-tree.levelOrder(func);
 //function to print the tree
 function prettyPrint(node, prefix = "", isLeft = true) {
-  if (node === null) {
-    return;
-  }
-  if (node.rightChild !== null) {
-    prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  }
+  if (node === null) return;
+  prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
   console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-  if (node.leftChild !== null) {
+  if (node.leftChild !== null)
     prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  }
 }
-
 //functions for random number
 function arrayOfRandom(size) {
   let array = new Array(size);
   for (let x = 0; x < size; x++) {
     let temp = Math.round(Math.random() * 100 + 1);
     array[x] = temp;
-  }
-  return array;
-}
-
-//function to return middle number, then recursively return middle of each side till end
-function split(array) {
-  if (array.length === 0) return null;
-  if (array.length === 1) return nodeFactory(array[0], null, null);
-  //if (array.length === 2) return nodeFactory(array[1], array[0], null);
-  let root = Math.round(array.length / 2 - 1);
-  let left = [],
-    right = [];
-  for (let x = 0; x < root; x++) left.push(array[x]);
-  for (let y = root + 1; y < array.length; y++) right.push(array[y]);
-  return nodeFactory(array[root], split(left), split(right));
-}
-
-//functions to sort array
-function mergeSort(unsorted) {
-  let array1, array2;
-  if (unsorted.length > 1) {
-    array1 = mergeSort(unsorted.slice(0, unsorted.length / 2));
-    array2 = mergeSort(unsorted.slice(unsorted.length / 2));
-  } else return unsorted;
-  return merge(array1, array2);
-}
-function merge(insertArray, array) {
-  let index = 0;
-  for (x in insertArray) {
-    while (insertArray[x] > array[index]) index++;
-    if (insertArray[x] !== array[index])
-      array = [...array.slice(0, index), insertArray[x], ...array.slice(index)];
   }
   return array;
 }
--- tst.js
+++ tst.js
@@ -1,17 +1,6 @@
-let x = true;
-let y = false;
-if (x ^ y) console.log(true);
-else console.log(false);
-
-let queue = new Array();
-
-queue.push(3);
-queue.push(45);
-queue.push(453);
-
-console.log(queue.shift());
-console.log(queue.length);
-while (queue.length > 0) {
-  queue.shift();
-  console.log("shift");
-}
+let h = 0;
+let x = h++;
+let y = ++h;
+console.log(h);
+console.log(x);
+console.log(y);
